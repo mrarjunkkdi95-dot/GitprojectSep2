472a633b706edf22632f65af1d9c17aa368b10a9
mrarjunkkdi95-dot <mrarjunkkdi95@gmail.com>
2026-09-18 16:45:20 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,6 @@ public class Employee{
     system.out.printIn("Employee Name is : karan");
  }
 public void empId(){
-   system.out.printIn("Employee Id is : 209");
+   system.out.printIn("Employee Id is : 210");
 }
 }
